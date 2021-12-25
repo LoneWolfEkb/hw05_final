877b0c82686a075e5c6de00b7e8404a024cfd047
Михаил <mturkhan@yandex.ru>
2021-12-25 07:46:36 +0500

    slash fix

--- yatube/posts/tests/test_routes.py
+++ yatube/posts/tests/test_routes.py
@@ -17,7 +17,7 @@ class RoutesModelTest(TestCase):
             [f'/posts/{POST_ID}/', 'post_detail', [POST_ID]],
             ['/create/', 'post_create', None],
             [f'/posts/{POST_ID}/edit/', 'post_edit', [POST_ID]],
-            [f'posts/{POST_ID}/comment', 'add_comment', [POST_ID]],
+            [f'/posts/{POST_ID}/comment', 'add_comment', [POST_ID]],
             [f'/follow/', 'follow_index', None],
             [f'/profile/{USERNAME}/follow/', 'profile_follow', [USERNAME]],
             [f'/profile/{USERNAME}/unfollow/', 'profile_unfollow', [USERNAME]]
