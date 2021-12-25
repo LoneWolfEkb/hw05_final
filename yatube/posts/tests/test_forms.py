f69201461e42589ee33e2369e61d1d5528f7cb91
Михаил <mturkhan@yandex.ru>
2021-12-26 00:23:09 +0500

    yt

--- yatube/posts/tests/test_forms.py
+++ yatube/posts/tests/test_forms.py
@@ -24,7 +24,7 @@ LOGIN_URL = reverse('users:login')
 
 TEMP_MEDIA_ROOT = tempfile.mkdtemp(dir=settings.BASE_DIR)
 
-small_gif = ( 
+small_gif = (
     b'\x47\x49\x46\x38\x39\x61\x02\x00'
     b'\x01\x00\x80\x00\x00\x00\x00\x00'
     b'\xFF\xFF\xFF\x21\xF9\x04\x00\x00'
